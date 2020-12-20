b4d7c24755b6141dcfcfc5fb72a5cb3947d15d48
deepan <deepan.c@gini.net>
2020-12-20 08:14:19 +0530

    Softmax is not effective. Reverting to ReLU + Adam

--- visualizer/architectures/unet_pruned.py
+++ visualizer/architectures/unet_pruned.py
@@ -88,7 +88,7 @@ class Unet_pruned(nn.Module):
                 padding=1,
                 output_padding=1,
             ),
-            nn.Softmax(dim=1),
+            nn.ReLU(),
         )
         return block
 
