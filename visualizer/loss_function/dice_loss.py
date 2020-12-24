7385e07de7382844c03a0954f158e43d6ece61e8
deepan <deepan.c@gini.net>
2020-12-24 09:03:20 +0530

    Added softmax activation at the end

--- visualizer/loss_function/dice_loss.py
+++ visualizer/loss_function/dice_loss.py
@@ -3,12 +3,11 @@ from torch.nn import functional as F
 import gin
 
 def get_dice_coefficient(input, target, smooth=1.0):
-    input = F.softmax(input, dim=1)
+    # input = F.softmax(input, dim=1) # If activation is ReLU
     input = input.contiguous()
     target = target.contiguous()
     intersection = (input * target).sum(dim=2).sum(dim=2)
-    dice_coeff = ((2.0 * intersection + smooth)
-            / (input.sum(dim=2).sum(dim=2) + target.sum(dim=2).sum(dim=2) + smooth))
+    dice_coeff = ((2.0 * intersection + smooth) / ((input**2 + target**2).sum(dim=2).sum(dim=2) + smooth))
     return dice_coeff
 
 @gin.configurable
