a9d0939eba815831135e0285e7dbf347a76ed71d
anirban.ghosh <anirban.ghosh@scai.fraunhofer.de>
2021-02-01 21:45:47 +0100

    change 1

--- visualizer/train.py
+++ visualizer/train.py
@@ -1,6 +1,9 @@
 import logging
 import click
 import gin
+gin.parse_config_file('config.gin')
+import use_gin
+print(use_gin.my_other_func())
 import torch
 
 from visualizer.utils import setup_logging
